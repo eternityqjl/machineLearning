40512bc9c0863d8bd4832cc23877198dd68a5dc4
eternityqjl <eternityqjl@gmail.com>
2021-05-17 11:40:59 +0800

    update gitignore

--- CNN/image_process.py
+++ CNN/image_process.py
@@ -1,5 +1,11 @@
 import cv2
-im = cv2.imread('test.jpg')
-imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+ 
+img = cv2.imread(r'C:\Users\83621\Documents\vscode\machineLearning\CNN\DSCF7691.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
+ 
+contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img,contours,-1,(0,0,255),3)
+ 
+cv2.imshow("img", img)
+cv2.waitKey(0)
